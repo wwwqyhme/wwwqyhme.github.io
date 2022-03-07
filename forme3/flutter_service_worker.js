9ec2e964bde0174f3722b043671f5f1311fc74ec
wwwqyhme <wwwqyhme@gmail.com>
2022-03-07 14:34:00 +0800

    3.6.0

--- forme3/flutter_service_worker.js
+++ forme3/flutter_service_worker.js
@@ -17,9 +17,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "dc7a2fbc1ff3b78cbcb18e97f0f79223",
-"/": "dc7a2fbc1ff3b78cbcb18e97f0f79223",
-"main.dart.js": "c437baf220151ace04ea3c8fe7d19aec",
+"index.html": "be6a73f08626e0b5b526eb9a5741ea95",
+"/": "be6a73f08626e0b5b526eb9a5741ea95",
+"main.dart.js": "47928c1bc6c1a17e0bd986a6f2e2173a",
 "manifest.json": "4a751f1f48979e741c98760074036e15",
 "version.json": "c36eb7f1db0bdf460f2a010a2cf344f6"
 };
